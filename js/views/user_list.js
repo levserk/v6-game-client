735f3e5293c018ddaa4c41c67824b2f9ba3e5b1a
Vlad <riosteyn@gmail.com>
2014-12-09 13:23:25 +0300

    add counters, add different tabs(free, inGame), some style

--- js/views/user_list.js
+++ js/views/user_list.js
@@ -1,7 +1,20 @@
 var UserListView = Backbone.View.extend({
     tpl: _.template($('#userListTpl').html()),
     events: {
-        'click .inviteBtn': 'invitePlayer'
+        'click .inviteBtn': 'invitePlayer',
+        'click .tabs div': 'clickTab'
+    },
+    clickTab: function(e) {
+        var target = $(e.currentTarget),
+            clickedTabName = target.attr('data-type');
+
+        if (clickedTabName === this.currentActiveTabName) {
+            return;
+        }
+
+        this.currentActiveTabName = clickedTabName;
+        this._setActiveTab(this.currentActiveTabName);
+        this._showPlayerListByTabName(this.currentActiveTabName);
     },
     invitePlayer: function(e) {
         var target = $(e.currentTarget),
@@ -24,18 +37,51 @@ var UserListView = Backbone.View.extend({
         console.log('invite user', userId);
     },
     initialize: function() {
+        /*
+        tabType: {'free', 'inGame'}
+         */
         this.ACTIVE_INVITE_CLASS = 'activeInviteBtn';
+        this.ACTIVE_TAB_CLASS = 'activeTab';
+        this.$listWrapper = this.$el.find('.tableWrap');
+        this.$counterFree = this.$el.find('.tabs div[data-type="free"]').find('span');
+        this.$counterinGame = this.$el.find('.tabs div[data-type="inGame"]').find('span');
         this.listenTo(client.userList, 'new_user', this.render.bind(this));
         this.listenTo(client.userList, 'leave_user', this.render.bind(this));
         this.listenTo(client.inviteManager, 'reject_invite', this.onRejectInvite.bind(this));
+
+        this.currentActiveTabName = 'free';
+        this._setActiveTab(this.currentActiveTabName);
+    },
+    _setActiveTab: function(tabName) {
+        this.$el.find('.tabs div').removeClass(this.ACTIVE_TAB_CLASS);
+        this.$el.find('.tabs div[data-type="' + tabName + '"]').addClass(this.ACTIVE_TAB_CLASS);
+    },
+    _setCounters: function() {
+        this.$counterFree.html('(' + this.$counterFree.find('table tr').length + ')');
+    },
+    _showPlayerListByTabName: function(tabName) {
+        // default
+        if (tabName === undefined) {
+            tabName = this.currentActiveTabName;
+        }
+
+        if (tabName === 'free') {
+            this.$listWrapper.html(this.tpl({
+                users: client.userList.getUsers()
+            }));
+        }
+        else if (tabName === 'inGame') {
+            this.$listWrapper.html('Нет игроков с такими параметрами')
+        } else {
+            console.warn('unknown tab', tabName);
+        }
     },
     onRejectInvite: function(invite) {
         this.$el.find('.' + this.ACTIVE_INVITE_CLASS + '[data-userId="' + invite.user.userId + '"]').html('Пригласить').removeClass(this.ACTIVE_INVITE_CLASS);
     },
     render: function() {
-        this.$el.find('.tableWrap').html(this.tpl({
-            users: client.userList.getUsers()
-        }));
+        this._showPlayerListByTabName();
+        this._setCounters();
         return this;
     }
 });

35e76146e324de4ca3f3bbfafb00934621021f06
Vlad <riosteyn@gmail.com>
2014-12-19 14:17:15 +0300

    user_list view disconnect

--- app/views/user_list.js
+++ app/views/user_list.js
@@ -10,9 +10,17 @@ define(['underscore', 'backbone', 'jquery',
         tplMain: _.template(tplMain),
         events: {
             'click .inviteBtn': 'invitePlayer',
-            'click .tabs div': 'clickTab'
+            'click .tabs div': 'clickTab',
+            'click .disconnectButton': '_reconnect'
+        },
+        _reconnect: function() {
+            this.client.socket.init();
         },
         clickTab: function(e) {
+            if (!this.client.socket.isConnected) {
+                return;
+            }
+
             var target = $(e.currentTarget),
                 clickedTabName = target.attr('data-type');
 
@@ -21,8 +29,7 @@ define(['underscore', 'backbone', 'jquery',
             }
 
             this.currentActiveTabName = clickedTabName;
-            this._setActiveTab(this.currentActiveTabName);
-            this._showPlayerListByTabName(this.currentActiveTabName);
+            this.render();
         },
         invitePlayer: function(e) {
             var target = $(e.currentTarget),
@@ -45,7 +52,15 @@ define(['underscore', 'backbone', 'jquery',
             console.log('invite user', userId);
         },
         initialize: function(_client) {
+            var bindedRender = this.render.bind(this);
+
             this.client = _client;
+
+            this.$disconnectedTab = $('<tr class="disconnected"><td><div>' +
+                '<span class="disconnectText">Соединение с сервером отсутствует</span>' +
+                '<br>' +
+                '<span class="disconnectButton">Переподключиться</span>' +
+                '</div></td></tr>');
             /*
              tabType: {'free', 'inGame'}
              */
@@ -59,11 +74,13 @@ define(['underscore', 'backbone', 'jquery',
             this.$counterFree = this.$el.find('.tabs div[data-type="free"]').find('span');
             this.$counterinGame = this.$el.find('.tabs div[data-type="inGame"]').find('span');
 
-            this.listenTo(this.client.userList, 'new_user', this.render.bind(this));
-            this.listenTo(this.client.userList, 'leave_user', this.render.bind(this));
+            this.listenTo(this.client.userList, 'new_user', bindedRender);
+            this.listenTo(this.client.userList, 'leave_user', bindedRender);
             this.listenTo(this.client.inviteManager, 'reject_invite', this.onRejectInvite.bind(this));
-            this.listenTo(this.client.userList, 'new_room', this.render.bind(this));
-            this.listenTo(this.client.userList, 'close_room', this.render.bind(this));
+            this.listenTo(this.client.userList, 'new_room', bindedRender);
+            this.listenTo(this.client.userList, 'close_room', bindedRender);
+            this.listenTo(this.client.socket, 'failed', bindedRender);
+            this.listenTo(this.client.socket, 'disconnection', bindedRender);
 
             this.currentActiveTabName = 'free';
             this._setActiveTab(this.currentActiveTabName);
@@ -73,27 +90,34 @@ define(['underscore', 'backbone', 'jquery',
             this.$el.find('.tabs div[data-type="' + tabName + '"]').addClass(this.ACTIVE_TAB_CLASS);
         },
         _setCounters: function() {
-            // TODO
+            if (!this.client.socket.isConnected) {
+                this.$counterFree.html('');
+                this.$counterinGame.html('');
+                return;
+            }
+
             this.$counterFree.html('(' + this.client.userList.getUserList().length + ')');
             this.$counterinGame.html('(' + this.client.userList.getRoomList().length * 2 + ')');
         },
-        _showPlayerListByTabName: function(tabName) {
+        _showPlayerListByTabName: function() {
             // default
-            if (tabName === undefined) {
-                tabName = this.currentActiveTabName;
+
+            if (!this.client.socket.isConnected) {
+                this.$list.html(this.$disconnectedTab);
+                return;
             }
 
-            if (tabName === 'free') {
+            if (this.currentActiveTabName === 'free') {
                 this.$list.html(this.tplFree({
                     users: this.client.userList.getUserList()
                 }));
             }
-            else if (tabName === 'inGame') {
+            else if (this.currentActiveTabName === 'inGame') {
                 this.$list.html(this.tplInGame({
                     rooms: this.client.userList.getRoomList()
                 }));
             } else {
-                console.warn('unknown tab', tabName);
+                console.warn('unknown tab', this.currentActiveTabName);
             }
         },
         onRejectInvite: function(invite) {

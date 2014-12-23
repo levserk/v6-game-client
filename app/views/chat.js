be7a82007376816338c603bca9327841d804b6a1
Vlad <riosteyn@gmail.com>
2014-12-23 16:18:01 +0300

    tabs, del msg, render msg

--- app/views/chat.js
+++ app/views/chat.js
@@ -1,7 +1,7 @@
 define(['underscore', 'backbone', 'text!tpls/v6-chatMain.ejs', 'text!tpls/v6-chatMsg.ejs'],
     function(_, Backbone, tplMain, tplMsg) {
         'use strict';
-        var TEST_DATA = [
+        var pub = [
             {
                 user: {
                     userId: 1,
@@ -9,48 +9,90 @@ define(['underscore', 'backbone', 'text!tpls/v6-chatMain.ejs', 'text!tpls/v6-cha
                 },
                 msg: {
                     msgId: 1,
-                    msgText: 'sex drugs rock!'
+                    msgText: 'Привет ребята!!',
+                    time: '5:45'
                 }
             },
             {
                 user: {
-                    userId: 1,
+                    userId: 55555555555,
                     userName: '50 cent'
                 },
                 msg: {
-                    msgId: 1,
+                    msgId: 52,
                     msgText: '50 cent!'
                 }
             },
             {
                 user: {
-                    userId: 1,
+                    userId: 10,
                     userName: 'fan'
                 },
                 msg: {
-                    msgId: 1,
+                    msgId: 498,
                     msgText: 'hi, fifty!!!'
                 }
             },
             {
                 user: {
-                    userId: 1,
+                    userId: 15646,
                     userName: '50 cent'
                 },
                 msg: {
-                    msgId: 1,
+                    msgId: 12,
                     msgText: 'fuck you'
                 }
             }
         ];
+        var priv = [
+            {
+                user: {
+                    userId: 1,
+                    userName: 'viteck'
+                },
+                msg: {
+                    msgId: 1,
+                    msgText: 'Привет!',
+                    time: '5:46'
+                }
+            },
+            {
+                user: {
+                    userId: 55555555555,
+                    userName: '50 cent'
+                },
+                msg: {
+                    msgId: 52,
+                    msgText: 'yoyoyo!'
+                }
+            }
+        ];
+        var TEST_DATA = {
+            pub: pub,
+            priv: priv
+        };
+
         var ChatView = Backbone.View.extend({
             tagName: 'div',
             id: 'v6Chat',
             tplMain: _.template(tplMain),
             tplMsg: _.template(tplMsg),
             events: {
+                'click .chatMsg': '_deleteMsg',
+                'click .tab': 'clickTab'
             },
             clickTab: function(e) {
+                var $target = $(e.target),
+                    tabName = $target.attr('data-type');
+
+                if (tabName === this.currentActiveTabName) {
+                    return;
+                }
+
+                this.currentActiveTabName = tabName;
+                this._setActiveTab(this.currentActiveTabName);
+
+                this._addAllMsgs(this.currentActiveTabName === 'public'? TEST_DATA.pub: TEST_DATA.priv);
             },
             invitePlayer: function(e) {
             },
@@ -58,14 +100,19 @@ define(['underscore', 'backbone', 'text!tpls/v6-chatMain.ejs', 'text!tpls/v6-cha
                 this.$el.html(this.tplMain());
 
                 this.CLASS_DISABLED = 'disabled';
+                this.CLASS_DELETE_CHAT_MESSAGE = 'delete';
+                this.ACTIVE_TAB_CLASS = 'activeTab';
+
                 this.$spinnerWrap = $('<li class="spinnerWrap"><div class="spinner"></div></li>');
                 this.$messagesWrap = this.$el.find('.messagesWrap');
                 this.$msgsList = this.$messagesWrap.find('ul');
 
+                this.currentActiveTabName = 'public';
+                this._setActiveTab(this.currentActiveTabName);
+
                 $('body').append(this.el);
-                debugger;
-                this._addAllMsgs();
-                this._setLoadingState();
+                this._addAllMsgs(TEST_DATA.pub);
+                //this._setLoadingState();
 
                 window.view = this;
             },
@@ -76,8 +123,37 @@ define(['underscore', 'backbone', 'text!tpls/v6-chatMain.ejs', 'text!tpls/v6-cha
             render: function() {
                 return this;
             },
+            _deleteMsg: function(e) {
+                // delete by id or as click .delete handler
+                var $msg, msgId;
+
+                if (!isNaN(+e) && typeof +e === 'number') {
+                    msgId = e;
+                } else {
+                    //клик не по кнопке удалить
+                    if (!$(e.target).hasClass(this.CLASS_DELETE_CHAT_MESSAGE)) {
+                        return;
+                    }
+
+                    $msg = $(e.currentTarget);
+                    msgId = $msg.attr('data-msgId')
+                }
+
+                // если был передан id сообщения
+                if (!$msg) {
+                    $msg = this.$el.find('li[data-msgId="' + msgId + '"]').remove();
+                }
+
+                if (!$msg) {
+                    console.warn('cannot find msg with  id', msgId, e);
+                    return;
+                }
+
+                $msg.remove();
+            },
             _addAllMsgs: function(msgs) {
-                _.each(TEST_DATA, function(msg) {
+                this.$msgsList.empty();
+                _.each(msgs, function(msg) {
                     this._addOneMsg(msg);
                 }, this);
             },
